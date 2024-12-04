0181c38789d1b447aa46c08257562260ce80dc84
Gabe Coletta <houfmtub@students.codeinstitute.net>
2024-12-04 20:48:22 +0000

    Added player choices to JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 // Initialize the player and computer scores
 let playerScore = 0;
 let computerScore = 0;
+// Available player choices
+function playGame(playerChoice) {
+    const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']};
