d66ea6014de62562f5f06fa391f0e3b5697455e7
Gabe Coletta <houfmtub@students.codeinstitute.net>
2024-12-04 20:52:05 +0000

    Added randomised computer choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,7 @@ let playerScore = 0;
 let computerScore = 0;
 // Available player choices
 function playGame(playerChoice) {
-    const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']};
+    const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+};
+// Randomly select a computer choice
+const computerChoice = choices[Math.floor(Math.random() * choices.length)];
